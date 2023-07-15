ca3ca880ac4658f97c81a92af475d900b8bcff06
Sharakush <kushwahashraddha147@gmail.com>
2023-07-15 16:44:14 +0530

    files Added

---
+++ Assets/script/LinkManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void openLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/shraddha-kushwaha-4806aa257/");
+    }
+     public void openGithub()
+    {
+        Application.OpenURL("https://github.com/Sharakush");
+    }
+  public void opentwitter()
+    {
+        Application.OpenURL("https://twitter.com/Shara_kush_01");
+    }
+  public void openInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/shara_kush_/");
+    }
+  public void openYoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/channel/UC9cEt9JO8y66lgjSEfcSi8w");
+    }
+ 
+ 
+}
